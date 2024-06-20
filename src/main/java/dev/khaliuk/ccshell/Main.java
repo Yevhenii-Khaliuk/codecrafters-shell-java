754809da7862faee8ee0aa685599c711dacddb62
Yevhenii Khaliuk <yevhenii.khaliuk@gmail.com>
2024-06-20 18:44:55 +0200

    Handle missing commands

--- src/main/java/dev/khaliuk/ccshell/Main.java
+++ src/main/java/dev/khaliuk/ccshell/Main.java
@@ -2,11 +2,14 @@ package dev.khaliuk.ccshell;
 
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+
+        System.out.printf("%s: command not found%n", input);
     }
 }
