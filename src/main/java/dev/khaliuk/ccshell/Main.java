a7e0b4a9009487d29be5bbd03aa4ad2976a7fdd4
Yevhenii Khaliuk <yevhenii.khaliuk@gmail.com>
2024-06-20 18:53:38 +0200

    REPL

--- src/main/java/dev/khaliuk/ccshell/Main.java
+++ src/main/java/dev/khaliuk/ccshell/Main.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
-
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
 
-        System.out.printf("%s: command not found%n", input);
+        while (true) {
+            System.out.print("$ ");
+            String input = scanner.nextLine();
+
+            System.out.printf("%s: command not found%n", input);
+        }
     }
 }
